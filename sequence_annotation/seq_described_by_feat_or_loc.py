ed8950ba0e199498340c54e8dc5eba0b0cc7b69d
Mac Alexandru Pavilcu <alexandrupavilcu@gmail.com>
2019-06-06 18:58:38 +0300

    seqfeature, location_testing, seq_described

---
+++ sequence_annotation/seq_described_by_feat_or_loc.py
@@ -0,0 +1,6 @@
+from Bio.Seq import Seq
+from Bio.SeqFeature import SeqFeature, FeatureLocation
+example_parent = Seq("ACCGAGACGGCAAAGGCTAGCATAGGTATGAGACTTCCTTCCTGCCAGTGCTGAGGAACTGGGAGCCTAC")
+example_feature = SeqFeature(FeatureLocation(5, 18), type="gene", strand=-1)
+feature_seq = example_parent[example_feature.location.start:example_feature.location.end].reverse_complement()
+print(feature_seq)
